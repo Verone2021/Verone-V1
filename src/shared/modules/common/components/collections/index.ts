f68ae452545a59425dfa143299303c56328c84aa
Romeo Dos Santos <163727524+Verone2021@users.noreply.github.com>
2025-11-06 07:30:58 +0100

    refactor(common): Migrate 13 common components to shared/modules/common
    - Address (3): AddressEditSection, AddressInput, AddressSelector
    - Carrier (1): CarrierSelector
    - Collections (5): CollectionCreationWizard, CollectionEditModal, CollectionFormModal, CollectionGrid, CollectionImageUpload
    - Pricing (4): GoogleMerchantPriceEditor, PriceListFormModal, PriceListItemFormModal, SupplierVsPricingEditSection

---
+++ src/shared/modules/common/components/collections/index.ts
@@ -0,0 +1,5 @@
+export { CollectionCreationWizard } from './CollectionCreationWizard';
+export { CollectionEditModal } from './CollectionEditModal';
+export { CollectionFormModal } from './CollectionFormModal';
+export { CollectionGrid } from './CollectionGrid';
+export { CollectionImageUpload } from './CollectionImageUpload';
---
+++ src/shared/modules/common/components/pricing/index.ts
@@ -0,0 +1,4 @@
+export { GoogleMerchantPriceEditor } from './GoogleMerchantPriceEditor';
+export { PriceListFormModal } from './PriceListFormModal';
+export { PriceListItemFormModal } from './PriceListItemFormModal';
+export { SupplierVsPricingEditSection } from './SupplierVsPricingEditSection';
