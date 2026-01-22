b85c04d572edfc800fd2edfe842ea5a82997de60
verone2021 <veronebyromeo@gmail.com>
2026-01-22 18:47:50 +0100

    [BO-DASH-CLEANUP] remove: delete dashboard frontend components

--- apps/back-office/src/app/(protected)/dashboard/components/index.ts
+++
@@ -1,9 +0,0 @@
-// Dashboard Components - Exports centralisés
-
-export { DashboardTabs, TABS } from './dashboard-tabs';
-export type { DashboardTab } from './dashboard-tabs';
-
-export { ConfigurableKPICard } from './configurable-kpi-card';
-export { KPIGrid } from './kpi-grid';
-export { KPISelectorModal } from './kpi-selector-modal';
-export { ConfigurableDashboard } from './configurable-dashboard';
